3a4e74d1feaca85f18c4e29ff62a0a6de4d2a374
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-05-08 08:21:28 +0000

    管理人MVC実装(編集や削除)/退会機能実装

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,7 +1,38 @@
 class Admins::UsersController < ApplicationController
 
-  def index
+  before_action :authenticate_admin!
 
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+    redirect_to admins_user_path(@user)
+    else
+    @user = User.find(params[:id])
+    render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admins_users_path
+  end
+
+  def user_params
+    params.require(:user).permit(:is_deleted)
   end
 
 end
