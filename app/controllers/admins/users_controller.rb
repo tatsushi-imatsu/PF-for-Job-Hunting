f7d5587a90a4a110d4853dde01776b767df72880
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-05-23 08:16:43 +0000

    レイアウト・バリデーション修正

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -3,12 +3,12 @@ class Admins::UsersController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.page(params[:page])
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).per(5)
   end
 
   def edit
