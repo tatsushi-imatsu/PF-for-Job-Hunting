3c5cb7d1598f0d4b80dcc0b863b880311762277d
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-05-27 12:19:51 +0000

    本番環境最終確認

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,11 +3,8 @@ class NotificationMailer < ActionMailer::Base
 
   def send_confirm_to_user(user)
     @user = user
-    mail(
-      subject: "退会処理が完了しました。", # メールのタイトル
-      to: @user.email # 宛先
-    ) do |format|
-      format.text
-    end
+    # mail(
+    #   subject: "退会処理が完了しました。", # メールのタイトル
+    #   to: @user.email # 宛先
   end
 end
