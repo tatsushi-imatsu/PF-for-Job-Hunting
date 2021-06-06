9baf45aa7fc13fe5694b48c005578905571ffb23
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-06-06 08:28:08 +0000

    エラー専用画面実装

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -24,7 +24,7 @@ describe 'ユーザログイン前のテスト' do
         sign_up_link = find_all('a')[3].native.inner_text
         expect(page).to have_link sign_up_link, href: new_user_registration_path
       end
-  #   end
+    end
   # end
 
   describe 'アバウト画面のテスト' do
@@ -39,32 +39,24 @@ describe 'ユーザログイン前のテスト' do
     end
   end
 
-#   describe 'ヘッダーのテスト: ログインしていない場合' do
-#     before do
-#       visit root_path
-#     end
+  describe 'ヘッダーのテスト: ログインしていない場合' do
+    before do
+      visit root_path
+    end
 
-#     context '表示内容の確認' do
-#       it 'タイトルが表示される' do
-#         expect(page).to have_content 'Bookers'
-#       end
-#       it 'Homeリンクが表示される: 左上から1番目のリンクが「Home」である' do
-#         home_link = find_all('a')[1].native.inner_text
-#         expect(home_link).to match(/home/i)
-#       end
-#       it 'Aboutリンクが表示される: 左上から2番目のリンクが「About」である' do
-#         about_link = find_all('a')[2].native.inner_text
-#         expect(about_link).to match(/about/i)
-#       end
-#       it 'sign upリンクが表示される: 左上から3番目のリンクが「sign up」である' do
-#         signup_link = find_all('a')[3].native.inner_text
-#         expect(signup_link).to match(/sign up/i)
-#       end
-#       it 'loginリンクが表示される: 左上から4番目のリンクが「login」である' do
-#         login_link = find_all('a')[4].native.inner_text
-#         expect(login_link).to match(/login/i)
-#       end
-#     end
+    context '表示内容の確認' do
+      it 'タイトルが表示される' do
+        expect(page).to have_content 'Word Share'
+      end
+      it 'Homeリンクが表示される: ハンバーガーメニュー上から1番目のリンクが「Home」である' do
+        home_link = find_all('a')[1].native.inner_text
+        expect(home_link).to match(/home/i)
+      end
+      it 'Aboutリンクが表示される: ハンバーガーメニュー上から2番目のリンクが「How to share」である' do
+        about_link = find_all('a')[2].native.inner_text
+        expect(about_link).to match(/how to share/i)
+      end
+    end
 
 #     context 'リンクの内容を確認' do
 #       subject { current_path }
