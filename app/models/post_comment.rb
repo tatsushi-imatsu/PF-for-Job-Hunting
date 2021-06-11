480c4c88cac03052fd23f6e4a8f6503a8d62a3b0
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-06-11 05:03:01 +0000

    SNSボタン・文字数カウント・パスワード表示実装

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,7 +2,7 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { maximum: 100 }
 
   has_many :notifications, dependent: :destroy
 end
