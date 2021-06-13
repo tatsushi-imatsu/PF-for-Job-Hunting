c12e53bed1e28e70936de7459662a55b03934661
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-06-13 06:24:28 +0000

    Rspec更新

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -25,7 +25,7 @@ describe 'ユーザログイン前のテスト' do
         expect(page).to have_link sign_up_link, href: new_user_registration_path
       end
     end
-  # end
+  end
 
   describe 'アバウト画面のテスト' do
     before do
@@ -220,50 +220,28 @@ describe 'ユーザログイン前のテスト' do
       click_button 'ログイン'
     end
 
-#     context 'ヘッダーの表示を確認' do
-#       it 'タイトルが表示される' do
-#         expect(page).to have_content 'Bookers'
-#       end
-#       it 'Homeリンクが表示される: 左上から1番目のリンクが「Home」である' do
-#         home_link = find_all('a')[1].native.inner_text
-#         expect(home_link).to match(/home/i)
-#       end
-#       it 'Usersリンクが表示される: 左上から2番目のリンクが「Users」である' do
-#         users_link = find_all('a')[2].native.inner_text
-#         expect(users_link).to match(/users/i)
-#       end
-#       it 'Booksリンクが表示される: 左上から3番目のリンクが「Books」である' do
-#         books_link = find_all('a')[3].native.inner_text
-#         expect(books_link).to match(/books/i)
-#       end
-#       it 'log outリンクが表示される: 左上から4番目のリンクが「logout」である' do
-#         logout_link = find_all('a')[4].native.inner_text
-#         expect(logout_link).to match(/logout/i)
-#       end
-#     end
-#   end
-
-#   describe 'ユーザログアウトのテスト' do
-#     let(:user) { create(:user) }
-
-#     before do
-#       visit new_user_session_path
-#       fill_in 'user[name]', with: user.name
-#       fill_in 'user[password]', with: user.password
-#       click_button 'Log in'
-#       logout_link = find_all('a')[4].native.inner_text
-#       logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
-#       click_link logout_link
-#     end
-
-#     context 'ログアウト機能のテスト' do
-#       it '正しくログアウトできている: ログアウト後のリダイレクト先においてAbout画面へのリンクが存在する' do
-#         expect(page).to have_link '', href: '/home/about'
-#       end
-#       it 'ログアウト後のリダイレクト先が、トップになっている' do
-#         expect(current_path).to eq '/'
-#       end
+    context 'ヘッダーの表示を確認' do
+      it 'タイトルが表示される' do
+        expect(page).to have_content 'word share'
       end
     end
+  
+
+  describe 'ユーザログアウトのテスト' do
+    let(:user) { create(:user) }
+
+    before do
+      logout_link = find_all('a')[3].native.inner_text
+      logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+      click_link logout_link
+    end
+
+    context 'ログアウト機能のテスト' do
+      it 'ログアウト後のリダイレクト先が、トップになっている' do
+        expect(current_path).to eq '/'
+      end
+    end
+   end
   end
+ end
 end
