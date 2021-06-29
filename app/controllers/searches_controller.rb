50220fc7271111fca32295d75c9759981b7892a5
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-06-29 13:09:47 +0000

    いいね・問い合わせ・検索MVCコメント更新

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,9 @@
 class SearchesController < ApplicationController
-  # before_action :authenticate_user!
 
   def search
     @range = params[:range]
     @range = "Post"
+    # rangeにユーザはあえて実装しない。投稿内容からユーザーと繋がるサイトコンセプトのため
     @posts = Post.looks(params[:search], params[:word]).page(params[:page]).per(5)
   end
 end
