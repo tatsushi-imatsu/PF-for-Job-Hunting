7816eb06cd4a81b85258d2f1e27cd44131e27f34
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-07-01 13:10:02 +0000

    バッチ・例外処理コメント更新

--- app/mailers/scheduled_processing_mailer.rb
+++ app/mailers/scheduled_processing_mailer.rb
@@ -3,12 +3,16 @@ class ScheduledProcessingMailer < ApplicationMailer
   def check_notice_mail
     
     users_with_unckecked_notices = User.all.select do |user|
-      user.passive_notifications.where(visited_id: user.id, checked: false).count >= 3
+    # 通知を確認していないユーザを選出
+    user.passive_notifications.where(visited_id: user.id, checked: false).count >= 3
+    # false（通知未確認）が３件以上あるユーザ  
     end
 
     users_with_unckecked_notices_mails = users_with_unckecked_notices.pluck(:email)
+    # 通知未確認のユーザのメールアドレスを取り出す
 
     mail(subject: "未読の通知が3件以上あります", bcc: users_with_unckecked_notices_mails)
+    # bccで通知未確認ユーザへメール送信
   end
   
 end
