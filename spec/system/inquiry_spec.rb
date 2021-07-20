caf03d6d0be033bcd66ef656e64592f1b5e8ddb3
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-07-20 12:19:42 +0000

    問い合わせRspec更新

--- spec/system/inquiry_spec.rb
+++ spec/system/inquiry_spec.rb
@@ -14,12 +14,23 @@ describe '問い合わせのテスト' do
       it "名前が表示されている" do
       expect(page).to have_content '名前'
       end
+      it "（必須）が表示されている" do
+      expect(page).to have_content '（必須）'
+      end
       it "メールアドレスが表示されている" do
       expect(page).to have_content 'メールアドレス'
       end
-      it "内容が表示されている" do
-      expect(page).to have_content '内容'
+      it "（必須）が表示されている" do
+      expect(page).to have_content '（必須）'
       end
-
+      it "内容（5文字以上入力してください。）が表示されている" do
+      expect(page).to have_content '内容（5文字以上入力してください。）'
+      end
+      it '確認ボタンが表示される' do
+      expect(page).to have_button '確認'
+      end
+      it "文字が表示されている" do
+      expect(page).to have_content '文字'
+      end
     end
 end
