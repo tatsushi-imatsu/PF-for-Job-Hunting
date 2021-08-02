e769309edf021d0a20b9fb7510d8ba1609f0e786
tatsushi-imatsu <tatsu0320itwl@gmail.com>
2021-08-02 22:28:31 +0000

    post_comment Rspec追加

--- spec/system/inquiry_spec.rb
+++ spec/system/inquiry_spec.rb
@@ -56,14 +56,14 @@ end
 # describe '問い合わせ（確認画面）のテスト' do
 #     before do
 #       visit inquiry_path
+#       fill_in 'name', with: Faker::Lorem.characters(number: 5)
+#       fill_in 'email', with: Faker::Internet.email
+#       fill_in 'message', with: Faker::Lorem.characters(number: 10)
+#       expect { click_button '確認' }.to change(inquiry.all, :count).by(1)
 #     end
 #       context '問い合わせ成功' do
 #       subject { current_path }
 #     it '正しく確認画面へ移行する' do
-#       fill_in Inquiry.name, with: Faker::Lorem.characters(number: 5)
-#       fill_in 'email', with: Faker::Internet.email
-#       fill_in 'message', with: Faker::Lorem.characters(number: 10)
-#       expect { click_button '確認' }.to change(inquiry.all, :count).by(1)
 #       expect(current_path).to eq '/inquiry/confirm'
 #     end
 #   end
